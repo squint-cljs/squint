c1b151681d18717b2435a02fa2db9571611bece6
Allen Rohner <arohner@gmail.com>
2010-02-19 15:25:31 -0600

    Lein-izing the build

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject scriptjure "0.1.1"
+  :description "a clojure DSL for generating javascript"
+  :url "http://github.com/arohner/scriptjure"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]])
