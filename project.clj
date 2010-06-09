24f5b74da37da2093a241aab0f716a2c39211090
Allen Rohner <arohner@gmail.com>
2010-06-09 17:50:33 -0500

    Bump the version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject scriptjure "0.1.8"
+(defproject scriptjure "0.1.9"
   :description "a clojure DSL for generating javascript"
   :url "http://github.com/arohner/scriptjure"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
