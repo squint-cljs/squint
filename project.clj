50e385953167f46971845d7ed6556fe1341c57de
Allen Rohner <arohner@gmail.com>
2010-08-16 10:34:45 -0500

    Update the dependencies to 1.2-RC3

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject scriptjure "0.1.11"
   :description "a clojure DSL for generating javascript"
   :url "http://github.com/arohner/scriptjure"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.2.0-RC3"]
+                 [org.clojure/clojure-contrib "1.2.0-RC2"]])
