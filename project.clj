f20f59bec0c6bb675c8c87ef6006bd744eda8bbb
Allen Rohner <arohner@gmail.com>
2010-09-13 15:38:36 -0500

    Fix a bug emitting keywords. Fix a bug with the unary operators

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject scriptjure "0.1.14"
+(defproject scriptjure "0.1.15"
   :description "a clojure DSL for generating javascript"
   :url "http://github.com/arohner/scriptjure"
   :dependencies [[org.clojure/clojure "1.2.0"]
