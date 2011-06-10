daa9663042ba633e15db98db55d6c8aa68fd60f2
Allen Rohner <arohner@gmail.com>
2011-06-10 11:20:57 -0500

    Bump the version, add lein-clojars as a dev-dep

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject scriptjure "0.1.22"
+(defproject scriptjure "0.1.23"
   :description "a clojure DSL for generating javascript"
   :url "http://github.com/arohner/scriptjure"
-  :dependencies [[org.clojure/clojure "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
