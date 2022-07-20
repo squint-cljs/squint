7c98cfbc009c6d8595c76549caa571bb50b184fe
Michiel Borkent <michielborkent@gmail.com>
2022-07-20 13:42:10 +0200

    separate top level by semi-colon

--- corpus/foo.cljs
+++ corpus/foo.cljs
@@ -2,3 +2,7 @@
 
 (js/console.log "hello")
 (js/console.log (+ 1 2 3))
+
+(let [x (do (js/console.log "in do")
+            12)]
+  (js/console.log "x + 1 =" (inc x)))
--- corpus/foo.mjs
+++ corpus/foo.mjs
@@ -1,2 +1,5 @@
-console.log("hello")
-console.log((1 + 2 + 3))
+console.log("hello");
+console.log((1 + 2 + 3));
+(function () { const x = (function () { console.log("in do");
+return 12; })();
+return (function () { return console.log("x + 1 =", (x + 1)); })(); })();
