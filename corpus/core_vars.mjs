b699c73ca4e1f7f84f45aea5cb2446efb7dbeeb0
Michiel Borkent <michielborkent@gmail.com>
2022-07-20 20:52:03 +0200

    core vars

--- corpus/core_vars.mjs
+++ corpus/core_vars.mjs
@@ -1,6 +1,7 @@
-import { toJs, get, keyword, assoc } from 'cherry-cljs/cljs.core.js'
+import { toJs, get, keyword, str, assoc } from 'cherry-cljs/cljs.core.js'
 
 const js_map = toJs(assoc(null, keyword("foo"), keyword("bar")));
 console.log(js_map);
-const clj_map = assoc(null, keyword("foo"), (1 + 2 + 3));
-console.log(get(clj_map, keyword("foo")));
+const clj_map = assoc(null, keyword("foo/bar"), (1 + 2 + 3));
+console.log(get(clj_map, keyword("foo/bar")));
+console.log(str(clj_map));
