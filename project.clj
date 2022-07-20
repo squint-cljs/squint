eab0fe35a59ab6d8fb7b35e4fef68aed7fdeeeec
Michiel Borkent <michielborkent@gmail.com>
2022-07-20 12:09:02 +0200

    add first test

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject scriptjure "0.1.24"
   :description "a clojure DSL for generating javascript"
   :url "http://github.com/arohner/scriptjure"
-  :dependencies [[org.clojure/clojure "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :dev-dependencies [[lein-clojars "0.6.0"]])
