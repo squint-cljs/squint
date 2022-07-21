0fa59f8853943b64ed27c6d3e985a1bec576e5a3
Michiel Borkent <michielborkent@gmail.com>
2022-07-21 23:20:42 +0200

    destructing

--- corpus/destructuring.cljs
+++ corpus/destructuring.cljs
@@ -2,5 +2,5 @@
 
 (let [^js {:keys [a] :as m} (clj->js {:a 1})
       ^js {:js/keys [b]} (js-obj "js/b" :js/b)
-      [c d e] [2 3 4]]
-  (prn m a b c d e))
+      [c d e & f] [2 3 4 5 6]]
+  (prn m a b c d e f))
