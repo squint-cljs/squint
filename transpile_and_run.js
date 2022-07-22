0059eca8133bf67764c79b6d6f315e3b9d2f06d8
Michiel Borkent <michielborkent@gmail.com>
2022-07-22 20:44:41 +0200

    clean

--- transpile_and_run.js
+++
@@ -1,8 +0,0 @@
-import { transpileFile } from './index.js';
-import { spawnSync } from 'child_process';
-
-const inFile = process.argv[2];
-
-const { outFile } = transpileFile({ inFile });
-
-await import(`./${outFile}`);
