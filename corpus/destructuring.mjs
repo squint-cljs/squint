4b230f22652bf69e9d63987c078d9bb00bd87437
Michiel Borkent <michielborkent@gmail.com>
2022-07-22 16:26:06 +0200

    fix async

--- corpus/destructuring.mjs
+++ corpus/destructuring.mjs
@@ -1,26 +1,25 @@
 import { seq, first, js_obj, prn, vector, __destructure_map, keyword, next, clj__GT_js, arrayMap } from 'cherry-cljs/cljs.core.js'
-
 (function () {
- let map__1123, m, a, map__1124, b, vec__1125, seq__1126, first__1127, c, d, e, f;
-map__1123 = clj__GT_js(arrayMap(keyword("a"), 1));
-map__1123 = __destructure_map(map__1123);
-m = map__1123;
-a = map__1123["a"];
-map__1124 = js_obj("js/b", keyword("js/b"));
-map__1124 = __destructure_map(map__1124);
-b = map__1124["js/b"];
-vec__1125 = vector(2, 3, 4, 5, 6);
-seq__1126 = seq(vec__1125);
-first__1127 = first(seq__1126);
-seq__1126 = next(seq__1126);
-c = first__1127;
-first__1127 = first(seq__1126);
-seq__1126 = next(seq__1126);
-d = first__1127;
-first__1127 = first(seq__1126);
-seq__1126 = next(seq__1126);
-e = first__1127;
-f = seq__1126;
+ let map__1, m, a, map__2, b, vec__3, seq__4, first__5, c, d, e, f;
+map__1 = clj__GT_js(arrayMap(keyword("a"), 1));
+map__1 = __destructure_map(map__1);
+m = map__1;
+a = map__1["a"];
+map__2 = js_obj("js/b", keyword("js/b"));
+map__2 = __destructure_map(map__2);
+b = map__2["js/b"];
+vec__3 = vector(2, 3, 4, 5, 6);
+seq__4 = seq(vec__3);
+first__5 = first(seq__4);
+seq__4 = next(seq__4);
+c = first__5;
+first__5 = first(seq__4);
+seq__4 = next(seq__4);
+d = first__5;
+first__5 = first(seq__4);
+seq__4 = next(seq__4);
+e = first__5;
+f = seq__4;
 return (function () {
  return prn(m, a, b, c, d, e, f);
 })();
