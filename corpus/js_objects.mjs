73df55a6a0e9ef1a2e5a60a16ab7428a717a1caa
Michiel Borkent <michielborkent@gmail.com>
2022-07-22 19:07:20 +0200

    run

--- corpus/js_objects.mjs
+++ corpus/js_objects.mjs
@@ -9,5 +9,15 @@ return (function () {
  return console.log((a + b));
 })();
 })();
+(function () {
+ let map__2, a, b;
+map__2 = { "a": 1, "b": (1 + 2 + 3) };
+map__2 = __destructure_map(map__2);
+a = map__2["a"];
+b = map__2["b"];
+return (function () {
+ return console.log((a + b));
+})();
+})();
 
 export {  }
