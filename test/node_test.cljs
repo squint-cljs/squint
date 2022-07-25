6b1386170d36078dee1105aa0a8ed7d1c25ad495
Michiel Borkent <michielborkent@gmail.com>
2022-07-25 16:10:36 +0200

    Add initial node test

---
+++ test/node_test.cljs
@@ -0,0 +1,17 @@
+(ns node-test
+  (:require
+   ["node:assert/strict" :as assert]
+   ["node:test" :refer [describe]]))
+
+(defn eq [x y]
+  (assert/deepEqual (clj->js x) (clj->js y)))
+
+(defn ^:async foo []
+  (let [x (-> (js/await (js/import "../corpus/fns.mjs"))
+              .-results
+              deref)]
+    (eq [3 3 3 0] x)))
+
+(describe
+ "fns works"
+ (js/await (foo)))
