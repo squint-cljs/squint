f5df76d75ed0a9751853d6f76fcb93c7a8a2b9a4
Michiel Borkent <michielborkent@gmail.com>
2022-07-30 22:06:31 +0200

    nicer

--- examples/react/index.mjs
+++ examples/react/index.mjs
@@ -5,50 +5,64 @@ import { useEffect } from 'https://cdn.skypack.dev/react';
 import * as rdom from 'https://cdn.skypack.dev/react-dom';
 var $ = (function () {
  let f1 = function (var_args) {
-let args27 = array();
-let len__22076__auto__8 = alength(arguments);
-let i39 = 0;
+let G__56 = alength(arguments);
+switch (G__56) {case 1:
+return f1.cljs$core$IFn$_invoke$arity$1((arguments[0]));
+break;
+case 2:
+return f1.cljs$core$IFn$_invoke$arity$2((arguments[0]), (arguments[1]));
+break;
+default:
+let args_arr38 = array();
+let len__22076__auto__9 = alength(arguments);
+let i410 = 0;
 while(true){
-if (truth_((i39 < len__22076__auto__8))) {
-args27.push((arguments[i39]));
-let G__10 = (i39 + 1);
-i39 = G__10;
+if (truth_((i410 < len__22076__auto__9))) {
+args_arr38.push((arguments[i410]));
+let G__11 = (i410 + 1);
+i410 = G__11;
 continue;
 };break;
 }
 ;
-let argseq__22197__auto__11 = ((2 < alength(args27))) ? (new IndexedSeq(args27.slice(2), 0, null)) : (null);
-return f1.cljs$core$IFn$_invoke$arity$variadic((arguments[0]), (arguments[1]), argseq__22197__auto__11);
+let argseq__22117__auto__12 = new IndexedSeq(args_arr38.slice(2), 0, null);
+return f1.cljs$core$IFn$_invoke$arity$variadic((arguments[0]), (arguments[1]), argseq__22117__auto__12);}
+};
+f1["cljs$core$IFn$_invoke$arity$1"] = function (elt) {
+return $(elt, null);
+};
+f1["cljs$core$IFn$_invoke$arity$2"] = function (elt, props) {
+return $(elt, props, null);
 };
 f1["cljs$core$IFn$_invoke$arity$variadic"] = function (elt, props, children) {
-let vec__1215 = (map_QMARK_(props)) ? (vector(clj__GT_js(props), children)) : (vector(null, cons(props, children)));
-let props16 = nth(vec__1215, 0, null);
-let children17 = nth(vec__1215, 1, null);
-let elt18 = (keyword_QMARK_(elt)) ? (name(elt)) : (elt);
-return react.createElement(elt18, props16, into_array(children17));
+let vec__1619 = (map_QMARK_(props)) ? (vector(clj__GT_js(props), children)) : (vector(null, cons(props, children)));
+let props20 = nth(vec__1619, 0, null);
+let children21 = nth(vec__1619, 1, null);
+let elt22 = (keyword_QMARK_(elt)) ? (name(elt)) : (elt);
+return react.createElement(elt22, props20, into_array(children21));
+};
+f1["cljs$lang$applyTo"] = function (seq13) {
+let G__1423 = first(seq13);
+let seq1324 = next(seq13);
+let G__1525 = first(seq1324);
+let seq1326 = next(seq1324);
+return this_as(self__22079__auto__, self__22079__auto__.cljs$core$IFn$_invoke$arity$variadic(G__1423, G__1525, seq1326));
 };
 f1["cljs$lang$maxFixedArity"] = 2;
-f1["cljs$lang$applyTo"] = function (seq4) {
-let G__519 = first(seq4);
-let seq420 = next(seq4);
-let G__621 = first(seq420);
-let seq422 = next(seq420);
-return this_as(self__22079__auto__, self__22079__auto__.cljs$core$IFn$_invoke$arity$variadic(G__519, G__621, seq422));
-};
 return f1;
 })();
 var App = function () {
 useEffect(function () {
 return confetti();
 }, vector());
-let vec__2326 = react.useState(0);
-let count27 = nth(vec__2326, 0, null);
-let setCount28 = nth(vec__2326, 1, null);
-return $(keyword("div"), $(keyword("p"), "You clicked ", count27, " times!"), $(keyword("button"), arrayMap(keyword("onClick"), function () {
+let vec__2730 = react.useState(0);
+let count31 = nth(vec__2730, 0, null);
+let setCount32 = nth(vec__2730, 1, null);
+return $(keyword("div"), $(keyword("p"), "You clicked ", count31, " times!"), $(keyword("button"), arrayMap(keyword("onClick"), function () {
 confetti();
-return setCount28((count27 + 1));
+return setCount32((count31 + 1));
 }), "Click me"));
 };
-rdom.render($(App, null, vector()), document.getElementById("app"));
+rdom.render($(App), document.getElementById("app"));
 
 export { $, App }
