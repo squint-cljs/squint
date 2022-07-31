6b5c9c79ff5413e7283be550011c539f7b09ebfb
Michiel Borkent <michielborkent@gmail.com>
2022-07-31 11:59:12 +0200

    deno fresh example

--- examples/deno-fresh/routes/[name].tsx
+++
@@ -1,7 +0,0 @@
-/** @jsx h */
-import { h } from "preact";
-import { PageProps } from "$fresh/server.ts";
-
-export default function Greet(props: PageProps) {
-  return <div>Hello {props.params.name}</div>;
-}
