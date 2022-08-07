9d3c8e7cd9b7a62b0b386d0296093bd7258ae6df
Michiel Borkent <michielborkent@gmail.com>
2022-08-07 21:20:17 +0200

    api

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-import { transpileFile as t} from  './lib/transpiler.js'
-import { js__GT_clj, keyword, clj__GT_js, str } from './cljs.core.js'
-
-function transpileFile({ inFile }) {
-  const resClj = t(js__GT_clj({"in-file": inFile}, keyword("keywordize-keys"), true));
-  const resJ = clj__GT_js(resClj);
-  return { outFile: resJ["out-file"]};
-}
-
-export { transpileFile }
+export { compileString } from 'lib/compiler.js'
