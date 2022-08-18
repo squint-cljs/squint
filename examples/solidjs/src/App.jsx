77a0fb029bfceb018ccefa858431246a5481264f
Michiel Borkent <michielborkent@gmail.com>
2022-08-18 16:22:44 +0200

    solidjs: test bundling

--- examples/solidjs/src/App.jsx
+++ examples/solidjs/src/App.jsx
@@ -1,4 +1,4 @@
-import { nth } from 'clavascript/core.js'
+import { nth, range } from 'clavascript/core.js'
 import styles from './App.module.css';
 import logo from './logo.svg';
 import { createSignal } from 'solid-js';
@@ -6,9 +6,9 @@ var Counter = function () {
 let vec__14 = createSignal(0);
 let counter5 = nth(vec__14, 0, null);
 let setCount6 = nth(vec__14, 1, null);
-return <div>Count: {(2 * counter5())} <button onClick={function () {
+return <div>Count: {range(counter5()).join(" ")} <div><button onClick={function () {
 return setCount6((counter5() + 1));
-}}>Click me</button></div>;
+}}>Click me</button></div></div>;
 };
 var App = function () {
 return <div class={styles.App}><header class={styles.header}><img src={logo} class={styles.logo} alt="logo"></img> <Counter></Counter></header></div>;
