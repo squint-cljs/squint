27cec3a4ff256f95f3088f37398dac1801baf99f
Michiel Borkent <michielborkent@gmail.com>
2022-08-20 21:18:07 +0200

    select-keys

--- core.js
+++ core.js
@@ -57,7 +57,7 @@ const OBJECT_TYPE = 3;
 const LIST_TYPE = 4;
 const SET_TYPE = 5;
 
-function newEmptyOfType(type) {
+function emptyOfType(type) {
   switch (type) {
     case MAP_TYPE:
       return new Map();
@@ -96,7 +96,7 @@ function assoc_in_with(f, fname, o, keys, value) {
     if (lastInChain instanceof Map) chainValue = lastInChain.get(k);
     else chainValue = lastInChain[k];
     if (!chainValue) {
-      chainValue = newEmptyOfType(baseType);
+      chainValue = emptyOfType(baseType);
     }
     chain.push(chainValue);
     lastInChain = chainValue;
@@ -555,4 +555,17 @@ export function mapcat(f, ...colls) {
 
 export function identity(x) {
   return x;
+}
+
+export function select_keys(o, ks) {
+  const typ = typeConst(o);
+  // ret could be object or array, but in the future, maybe we'll have an IEmpty protocol
+  const ret = emptyOfType(typ);
+  for (const k of ks) {
+    const v = get(o, k);
+    if (v != undefined) {
+      assoc_BANG_(ret, k, v);
+    }
+  }
+  return ret;
 }
--- test/clava/compiler_test.cljs
+++ test/clava/compiler_test.cljs
@@ -813,5 +813,15 @@
   (testing "multiple colls"
     (is (eq ["a" 1 "b" 2] (jsv! '(mapcat list [:a :b :c] [1 2]))))))
 
+(deftest select-keys-test
+  (is (eq {:a 1 :b 2} (jsv! '(select-keys {:a 1 :b 2 :c 3} [:a :b]))))
+  (is (jsv! '(let [m (select-keys (js/Map. [[:a 1] [:b 2] [:c 3]]) [:a :b])]
+               (and
+                (instance? js/Map m)
+                (= 1 (get m :a))
+                (= 2 (get m :b))
+                (not (.has m :c))))))
+  (is (eq [:a :b] (jsv! '(select-keys [:a :b :c] [0 1])))))
+
 (defn init []
   (cljs.test/run-tests 'clava.compiler-test))
