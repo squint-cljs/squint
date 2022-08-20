125ce026c95cb6b0fdb4725bca726078ff446258
Cora Sutton <cora@sutton.me>
2022-08-19 22:20:35 -0500

    Add a test for an empty list

--- test/clava/compiler_test.cljs
+++ test/clava/compiler_test.cljs
@@ -792,7 +792,9 @@
   (testing "creates a list of elements"
     (is (eq '(1 2 3) (jsv! '(list 1 2 3)))))
   (testing "accepts a single, numeric element in the list"
-    (is (eq '(23) (jsv! '(list 23))))))
+    (is (eq '(23) (jsv! '(list 23)))))
+  (testing "creates an empty list"
+    (is (eq '() (jsv! '(list))))))
 
 (deftest instance-test
   (is (true? (jsv! '(instance? js/Array []))))
