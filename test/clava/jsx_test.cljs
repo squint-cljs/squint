61d52117eef2b41b475ed02c8d56cbd13d3d15de
Michiel Borkent <michielborkent@gmail.com>
2022-08-27 11:19:04 +0200

    tests

--- test/clava/jsx_test.cljs
+++ test/clava/jsx_test.cljs
@@ -3,19 +3,18 @@
    [clava.test-utils :refer [eq js! jss! jsv!]]
    [clojure.string :as str]
    [clojure.test :as t :refer [async deftest is testing]]
-   ["eslint" :refer [Linter]]))
+   ["@babel/core" :refer [transformSync]]))
 
 (defn test-jsx [s]
-  (let [expr (jss! s)
-        _ (prn expr)
-        results (js->clj (.verify (new Linter) expr #js {:parserOptions #js {:ecmaFeatures #js {:jsx true}}}) :keywordize-keys true)]
-    (is (empty? results) results)))
+  (let [expr (jss! s)]
+    (transformSync expr #js {:presets #js ["@babel/preset-react"]})))
 
 
 (deftest jsx-test
   (test-jsx "#jsx [:a {:href \"http://foo.com\"}]")
   (test-jsx "#jsx [:div {:dangerouslySetInnerHTML {:_html \"<i>Hello</i>\"}}]")
-  (test-jsx "(defn App [] (let [x 1] #jsx [:div {:id x}]))"))
+  (test-jsx "(defn App [] (let [x 1] #jsx [:div {:id x}]))")
+  (is (thrown? js/Error (test-jsx "#jsx [:a {:href 1}]"))))
 
 
 
