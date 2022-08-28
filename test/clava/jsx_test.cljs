ea536c181fb11a6603215bc51cc27d85d01a9e99
Michiel Borkent <michielborkent@gmail.com>
2022-08-28 16:54:00 +0200

    Fix JSX tests + keyword in fn position (#166)

--- test/clava/jsx_test.cljs
+++ test/clava/jsx_test.cljs
@@ -1,20 +1,24 @@
 (ns clava.jsx-test
   (:require
-   [clava.test-utils :refer [eq js! jss! jsv!]]
-   [clojure.string :as str]
-   [clojure.test :as t :refer [async deftest is testing]]
-   ["@babel/core" :refer [transformSync]]))
+   ["@babel/core" :refer [transformSync]]
+   ["react" :as React]
+   [clava.test-utils :refer [jss!]]
+   [clojure.test :as t :refer [deftest is]]
+   [goog.object :as gobject]))
+
+(gobject/set js/global "React" React)
 
 (defn test-jsx [s]
-  (let [expr (jss! s)]
-    (transformSync expr #js {:presets #js ["@babel/preset-react"]})))
-
+  (let [expr (jss! s)
+        code (:code (js->clj (transformSync expr #js {:presets #js ["@babel/preset-react"]}) :keywordize-keys true))]
+    (js/eval code)))
 
 (deftest jsx-test
   (test-jsx "#jsx [:a {:href \"http://foo.com\"}]")
   (test-jsx "#jsx [:div {:dangerouslySetInnerHTML {:_html \"<i>Hello</i>\"}}]")
   (test-jsx "(defn App [] (let [x 1] #jsx [:div {:id x}]))")
-  (is (thrown? js/Error (test-jsx "#jsx [:a {:href 1}]"))))
+  (is (thrown? js/Error (test-jsx "#jsx [:a {:href 1}]")))
+  (test-jsx "(let [classes {:classes \"foo bar\"}] #jsx [:div {:className (:classes classes)}])"))
 
 
 
