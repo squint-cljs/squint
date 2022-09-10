f5a80296e64bab14d27baa5fef233f1b3db95129
Michiel Borkent <michielborkent@gmail.com>
2022-09-10 12:06:35 +0200

    squint

--- examples/solidjs/src/App.cljs
+++ examples/solidjs/src/App.cljs
@@ -1,12 +1,13 @@
 (ns App
   (:require ["./App.module.css$default" :as styles]
             ["./logo.svg$default" :as logo]
-            ["solid-js" :refer [createSignal]]))
+            ["solid-js" :refer [createSignal]]
+            [squint.string :as str]))
 
 (defn Counter []
   (let [[counter setCount] (createSignal 0)]
     #jsx [:div
-          "Count:" (.join (range (counter)) " ")
+          "Count:" (str/join " " (range (counter)))
           [:div
            [:button
             {:onClick (fn []
