eb2bd369b50b7648ce178e6fbd17870b5d36b3bd
Michiel Borkent <michielborkent@gmail.com>
2022-09-10 12:06:35 +0200

    squint

--- examples/solidjs/src/App.jsx
+++ examples/solidjs/src/App.jsx
@@ -1,4 +1,7 @@
-import { nth, range } from 'clavascript/core.js'
+import {  } from './App.module.css'
+import {  } from './logo.svg'
+import { join as str_join } from 'squint-cljs/string.js'
+import { nth, range } from 'squint-cljs/core.js'
 import styles from './App.module.css';
 import logo from './logo.svg';
 import { createSignal } from 'solid-js';
@@ -6,7 +9,7 @@ var Counter = function () {
 let vec__14 = createSignal(0);
 let counter5 = nth(vec__14, 0, null);
 let setCount6 = nth(vec__14, 1, null);
-return <div>Count: {range(counter5()).join(" ")} <div><button onClick={function () {
+return <div>Count: {str_join(" ", range(counter5()))} <div><button onClick={function () {
 return setCount6((counter5() + 1));
 }}>Click me</button></div></div>;
 };
