bdd9ee9cd64aa99915b6ffc6347ddf24eb51ed40
Michiel Borkent <michielborkent@gmail.com>
2022-10-21 20:19:10 +0200

    squint

--- examples/solidjs/src/App.jsx
+++ examples/solidjs/src/App.jsx
@@ -1,22 +1,27 @@
 import {  } from './App.module.css'
 import {  } from './logo.svg'
 import { join as str_join } from 'squint-cljs/string.js'
-import { nth, range } from 'squint-cljs/core.js'
+import { get, nth, range } from 'squint-cljs/core.js'
 import styles from './App.module.css';
 import logo from './logo.svg';
 import { createSignal } from 'solid-js';
-var Counter = function () {
-let vec__14 = createSignal(0);
-let counter5 = nth(vec__14, 0, null);
-let setCount6 = nth(vec__14, 1, null);
-return <div>Count: {str_join(" ", range(counter5()))} <div><button onClick={function () {
-return setCount6((counter5() + 1));
+var Counter = function (p__1) {
+let map__23 = p__1;
+let init4 = get(map__23, "init");
+let vec__58 = createSignal(init4);
+let counter9 = nth(vec__58, 0, null);
+let setCount10 = nth(vec__58, 1, null);
+return <div>Count: {str_join(" ", range(counter9()))} <div><button onClick={function () {
+return setCount10((counter9() + 1));
 }}>Click me</button></div></div>;
-};
+}
+;
 var App = function () {
-return <div class={styles.App}><header class={styles.header}><img src={logo} class={styles.logo} alt="logo"></img> <Counter></Counter></header></div>;
-};
-var default$ = App;
+return <div class={styles.App}><header class={styles.header}><img src={logo} class={styles.logo} alt="logo"></img> <Counter init={5}></Counter></header></div>;
+}
+;
+var default$ = App
+;
 
 export { Counter, App }
 export default default$
