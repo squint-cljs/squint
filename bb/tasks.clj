b13efa4127cfb4ed189d2f6528e52f9f271d1a38
Michiel Borkent <michielborkent@gmail.com>
2023-11-27 20:13:16 +0100

    let's try it

--- bb/tasks.clj
+++ bb/tasks.clj
@@ -1,7 +1,7 @@
 (ns tasks
   (:require
    [babashka.fs :as fs]
-   [babashka.process :refer [shell]]
+   [babashka.process :as p :refer [shell]]
    [cheshire.core :as json]
    [node-repl-tests]
    [clojure.string :as str]))
@@ -67,4 +67,22 @@
   (node-repl-tests/run-tests {})
   (test-project {}))
 
-
+(defn libtests []
+  #_(build-squint-npm-package)
+  (let [dir "libtests"]
+    (fs/delete-tree dir)
+    (fs/create-dir dir)
+    (shell {:dir dir} "git clone https://github.com/nextjournal/clojure-mode")
+    (let [dir (fs/path dir "clojure-mode")
+          shell (partial p/shell {:dir dir})
+          squint-local (fs/path dir "node_modules/squint-cljs")]
+      (fs/create-dirs dir)
+      (shell "yarn install")
+      (fs/delete-tree squint-local)
+      (fs/create-dirs squint-local)
+      (run! #(fs/copy % squint-local) (fs/glob "." "*.{js,json}"))
+      (fs/copy-tree "lib" (fs/path squint-local "lib"))
+      (fs/copy-tree "src" (fs/path squint-local "src"))
+      (shell "node_modules/squint-cljs/node_cli.js" "compile")
+      (shell "node dist/nextjournal/clojure_mode_tests.mjs")
+      (println "clojure-mode tests successful!"))))
