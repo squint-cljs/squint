614cf48a9b8e06472c4ffc1f69664b47d51785f6
Peter Strömberg <pez@pezius.com>
2023-12-10 12:18:19 +0100

    Back to `!==`

--- src/squint/set.js
+++ src/squint/set.js
@@ -2,10 +2,8 @@ import * as core from './core.js';
 
 function _bubble_max_key(k, coll) {
   const max = core.max_key(k, ...coll);
-  return [max, ...coll.filter(x => !core.identical_QMARK_(x, max))];
+  return [max, ...coll.filter(x => x !== max)];
 }
-
-export const __testing__bubble_max_key = _bubble_max_key;
 
 function _intersection2(x, y) {
   if (x.size > y.size) {
