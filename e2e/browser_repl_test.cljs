fa5d5f3dcc1ddff7b8e9b59c8fdb578b79b89b84
Michiel Borkent <michielborkent@gmail.com>
2026-06-02 14:01:23 +0200

    e2e: node nREPL server test + group e2e tests under one task (#834)

--- e2e/browser_repl_test.cljs
+++ e2e/browser_repl_test.cljs
@@ -6,139 +6,19 @@
   bencode TCP, and evaluates forms. Exercises the whole stack: compiler REPL
   output, /@resolve-deps ns fallback, nREPL bencode, vite-WS browser eval, and
   cross-ns redef visibility (the live-global alias binding)."
-  (:require ["node:net" :as net]
-            ["node:child_process" :as cp]
+  (:require ["node:child_process" :as cp]
             ["node:fs" :as fs]
             ["node:path" :as path]
             ["playwright$default" :as pw]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [nrepl-client :refer [make-client nrepl-request nrepl-eval msg-field with-timeout]]))
 
 (def PORT 5199)        ;; vite dev (isolated from the :5188 dev server)
 (def NREPL-PORT 1340)  ;; nREPL TCP (isolated from :1339)
 (def URL (str "http://localhost:" PORT "/"))
 (def EXDIR (path/resolve "examples/browser-repl"))
 
-;; ---------------------------------------------------------------- bencode ----
-
-(defn- bbuf [s] (js/Buffer.from s "utf8"))
-
-(defn bencode [x]
-  (cond
-    (number? x) (bbuf (str "i" x "e"))
-    (string? x) (let [b (js/Buffer.from x "utf8")]
-                  (js/Buffer.concat #js [(bbuf (str (.-length b) ":")) b]))
-    (array? x) (js/Buffer.concat (js/Array.from
-                                  (concat [(bbuf "l")] (map bencode x) [(bbuf "e")])))
-    :else ;; plain JS object -> dict with sorted keys
-    (let [ks (.sort (js/Object.keys x))
-          parts (mapcat (fn [k] [(bencode k) (bencode (aget x k))]) ks)]
-      (js/Buffer.concat (js/Array.from (concat [(bbuf "d")] parts [(bbuf "e")]))))))
-
-(declare bdecode)
-
-(defn- bdecode-coll [b i close-fn]
-  (loop [j (inc i) items #js []]
-    (cond
-      (>= j (.-length b)) nil
-      (= (aget b j) 0x65) (close-fn items (inc j))
-      :else (when-let [r (bdecode b j)]
-              (.push items (aget r 0))
-              (recur (aget r 1) items)))))
-
-(defn bdecode
-  "Decode one value at index `i` of buffer `b`. Returns #js [value end] or nil
-  when the buffer doesn't yet hold a complete value."
-  [b i]
-  (when (< i (.-length b))
-    (let [c (aget b i)]
-      (cond
-        (= c 0x69) ;; i<int>e
-        (let [e (.indexOf b 0x65 i)]
-          (when (>= e 0)
-            #js [(js/parseInt (.toString b "ascii" (inc i) e) 10) (inc e)]))
-        (= c 0x6c) ;; l..e
-        (bdecode-coll b i (fn [items end] #js [items end]))
-        (= c 0x64) ;; d..e
-        (bdecode-coll b i (fn [items end]
-                            (let [o #js {}]
-                              (loop [k 0]
-                                (when (< k (.-length items))
-                                  (aset o (aget items k) (aget items (inc k)))
-                                  (recur (+ k 2))))
-                              #js [o end])))
-        :else ;; <len>:<bytes>
-        (let [colon (.indexOf b 0x3a i)]
-          (when (>= colon 0)
-            (let [len (js/parseInt (.toString b "ascii" i colon) 10)
-                  s (inc colon)
-                  e (+ s len)]
-              (when (<= e (.-length b))
-                #js [(.toString b "utf8" s e) e]))))))))
-
-;; ------------------------------------------------------------ nREPL client ----
-
-(defn make-client [port]
-  (js/Promise.
-   (fn [resolve reject]
-     (let [sock (.connect net port "127.0.0.1")
-           buf (atom (js/Buffer.alloc 0))
-           handlers (atom [])]
-       (.on sock "error" reject)
-       (.on sock "data"
-            (fn [data]
-              (reset! buf (js/Buffer.concat #js [@buf data]))
-              (loop []
-                (when-let [r (bdecode @buf 0)]
-                  (reset! buf (.subarray @buf (aget r 1)))
-                  (doseq [h @handlers] (h (aget r 0)))
-                  (recur)))))
-       (.on sock "connect"
-            (fn [] (resolve #js {:sock sock :handlers handlers})))))))
-
-(defn nrepl-request
-  "Send an nREPL op, resolve with the vector of response messages once a `done`
-  status arrives for this id."
-  [client op]
-  (js/Promise.
-   (fn [resolve _reject]
-     (let [id (str (js/Math.random))
-           msgs #js []]
-       (aset op "id" id)
-       (swap! (.-handlers client) conj
-              (fn [msg]
-                (when (= (aget msg "id") id)
-                  (.push msgs msg)
-                  (when-let [st (aget msg "status")]
-                    (when (.includes st "done")
-                      (resolve msgs))))))
-       (.write (.-sock client) (bencode op))))))
-
-(defn msg-field
-  "First non-nil value of string key `k` across the response messages."
-  [msgs k]
-  (some (fn [m] (aget m k)) (js/Array.from msgs)))
-
-(defn nrepl-eval
-  ([client session code] (nrepl-eval client session code nil))
-  ([client session code ns]
-   (-> (nrepl-request client (cond-> #js {:op "eval" :session session :code code}
-                               ns (doto (aset "ns" ns))))
-       (.then (fn [msgs]
-                (some (fn [m] (aget m "value")) (js/Array.from msgs)))))))
-
 ;; --------------------------------------------------------------- helpers ----
-
-(defn with-timeout
-  "Reject with a labeled error if `p` doesn't settle within `ms`. Keeps the test
-  from hanging forever (e.g. in CI) when something never becomes ready."
-  [ms label p]
-  (js/Promise.race
-   #js [p
-        (js/Promise.
-         (fn [_ reject]
-           (js/setTimeout
-            (fn [] (reject (js/Error. (str "timeout after " ms "ms waiting for: " label))))
-            ms)))]))
 
 (defn wait-output
   "Resolve when `substr` appears in the stream's accumulated output. Accumulates
