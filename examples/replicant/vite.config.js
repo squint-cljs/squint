8453ee15ab4ef74190502d1b2300912016f22b16
Michiel Borkent <michielborkent@gmail.com>
2026-08-11 15:34:12 +0200

    bundle size

--- examples/replicant/vite.config.js
+++ examples/replicant/vite.config.js
@@ -1,9 +1,17 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import squint from 'squint-cljs/vite';
+import analyzer from 'rollup-plugin-analyzer';
+
+const { plugin: analyze } = analyzer;
 
 export default defineConfig(() => {
   return {
     plugins: [squint()],
+    build: {
+      rollupOptions: {
+        plugins: [analyze({ summaryOnly: true, limit: 15 })],
+      },
+    },
   };
 });
